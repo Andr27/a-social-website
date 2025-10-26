30647c97e98808c7a09fa4536f07bf8fcfe11e5e
Andr27 <Andr27chet04@gmail.com>
2025-10-26 16:14:06 +1000

    add change password

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -6,8 +6,18 @@ from . import views
 
 urlpatterns = [
     #path('login/', views.user_login, name='login'),
+    #url входа и выхода
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
+    #url смены пароля
+    path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+
+
+
+
     path('', views.dashboard, name='dashboard'),
+
+
 ]
