b188f0a9c77863775223844f8aa392803d7256bc
Andr27 <Andr27chet04@gmail.com>
2025-11-09 17:20:13 +1000

    Add likes

--- bookmarks/images/urls.py
+++ bookmarks/images/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
-
-
 from . import views
 
 
@@ -9,4 +7,10 @@ app_name = 'images'
 
 urlpatterns = [
     path('create/', views.image_create, name='create'),
+    path(
+        'detail/<int:id>/<slug:slug>/',
+        views.image_detail,
+        name='detail',
+    ),
+    path('like/', views.image_like, name='like'),
 ]
