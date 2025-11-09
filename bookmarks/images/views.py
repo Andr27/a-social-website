0842cb7b08b272b77deaa2016c7bcf853b2937d0
Andr27 <Andr27chet04@gmail.com>
2025-11-09 18:04:47 +1000

    Add POSTR PORKR

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -4,8 +4,9 @@ from django.contrib.auth.decorators import login_required
 from .forms import ImageCreateForm
 from .models import Image
 from django.shortcuts import get_object_or_404
-from django.http import JsonResponse
 from django.views.decorators.http import require_POST
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.http import HttpResponse, JsonResponse
 
 
 @login_required
@@ -50,3 +51,37 @@ def image_like(request):
         except Image.DoesNotExist:
             pass
     return JsonResponse({'status': 'error'})
+
+
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 10)
+    page = request.GET.get('page')
+    images_only = request.GET.get('images_only')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        images = paginator.page(1)
+    except EmptyPage:
+        if images_only:
+            return HttpResponse('')
+        images = paginator.page(paginator.num_pages)
+    if images_only:
+        return render(request, 'images/image/list_images.html', {'section': "images", 'images': images})
+
+    return render(request, 'images/image/list.html', {'section': "images", 'images': images})
+
+
+
+
+
+
+
+
+
+
+
+
+
